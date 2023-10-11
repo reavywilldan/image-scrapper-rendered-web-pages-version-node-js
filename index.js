0b689cfccc066a2c3f540a78e136253398f77864
reavywilldan <reavywilldan@gmail.com>
2023-10-11 10:16:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+const writeStream = fs.createWriteStream('image-link.txt', 'utf-8')
+const baseUrl = 'https://jadinikah.co/'
+
+puppeteer.launch().then(async (browser) => {
+    const page = await browser.newPage()
+    await page.goto(baseUrl)
+
+    console.log("page has been loaded!")
+
+    const imgSource = await page.evaluate(() => {
+        const srcs = Array.from(document.querySelectorAll("img")).map((image) => {
+            const baseUrl = 'https://jadinikah.co/'
+
+            const src = image.getAttribute('src')
+            const newBaseUrl = baseUrl.replace(/\/$/, '')
+
+            let link = ''
+
+            if (src.startsWith('https://')) {
+                link = src
+            } else {
+                link = `${newBaseUrl}${src}`
+            }
+
+            return link
+        })
+
+        return srcs
+    })
+
+    writeStream.write(imgSource.join('\n'))
+
+    await browser.close()
+}).catch((err) => {
+    console.log(err)
+})
